af6e34bcc400ebe8145c67c460c45f81c45e5e92
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-09-22 12:19:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,33 @@
    #Example 'main.tf':
      # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "example-organization"
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-organization"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "example-workspace"
-         }
-       }
-     }
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+#       }
+#     }
+
+
+provider "google" {
+   version = "3.53"
+   
+   
+   project = var.project
+   region  = var.region
+   zone    = var.region
+}
+
+resource "google_container_cluster" "my_vpc_native_cluster" {
+   name                 = var.gke_cluster_name
+   location             = var.zone
+   initial_node_count   = 1
+   
+   network              = "default"
+   subnetwork           = "default"
+  
+}
